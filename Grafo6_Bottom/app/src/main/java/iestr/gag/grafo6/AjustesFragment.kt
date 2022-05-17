1d4b0932e520091b3e2e446b9aa333d9419eb613
RakuKinG <63345441+RakuKinG@users.noreply.github.com>
2022-05-17 14:39:09 +0200

    arreglando cosas visuales

--- Grafo6_Bottom/app/src/main/java/iestr/gag/grafo6/AjustesFragment.kt
+++ Grafo6_Bottom/app/src/main/java/iestr/gag/grafo6/AjustesFragment.kt
@@ -5,21 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import iestr.gag.grafo6.databinding.FragmentAjustesBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AjustesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AjustesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var enlace: FragmentAjustesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,19 +31,17 @@ class AjustesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ajustes, container, false)
+        enlace= FragmentAjustesBinding.inflate(inflater, container,false)
+        return enlace.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        enlace.boton.setOnClickListener {
+            Toast.makeText(context, "V1.0.0", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AjustesFragment.
-         */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
--- Grafo6_Bottom/app/src/main/java/iestr/gag/grafo6/AyudaFragment.kt
+++ Grafo6_Bottom/app/src/main/java/iestr/gag/grafo6/AyudaFragment.kt
@@ -11,11 +11,6 @@ import android.view.ViewGroup
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AyudaFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AyudaFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -38,16 +33,6 @@ class AyudaFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AyudaFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
         fun newInstance(param1: String, param2: String) =
             AyudaFragment().apply {
                 arguments = Bundle().apply {
